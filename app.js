89931f7b73cb0ae08bdab8a9e523fa732ddfe9f3
ashrafomid2025 <omidmashraf@gmail.com>
2025-03-15 15:02:56 +0430

    hero added

--- app.js
+++ app.js
@@ -2,6 +2,28 @@ const clic = document.getElementById("clik");
 const navm = document.getElementById("navm");
 const orderul = document.getElementById("order-ul");
 const order = document.getElementById("order");
+const service = document.getElementById("service");
+const serviceM = document.getElementById("service-m");
+const contact = document.getElementById("contact");
+const constactM = document.getElementById("contact-m");
+contact.addEventListener("click",()=>{
+    if(constactM.style.display=="block"){
+        constactM.style.display = "none";
+    }
+    else{
+        constactM.style.display = "block"
+    }
+})
+service.addEventListener("click",()=>{
+    if(serviceM.style.display =="block"){
+        serviceM.style.display = "none"
+        
+    }
+    else{
+        serviceM.style.display = "block";
+    }
+});
+
 clic.addEventListener("click",()=>{
     if(navm.style.display =="block"){
         navm.style.display = "none"
